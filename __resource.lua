197aac7ab45ed212d0b60185c4841113e128ece7
Trickxqster <52986951+Trickxqster@users.noreply.github.com>
2019-07-30 13:39:52 +0300

    Version 1.0
    Cleaned the resource up.
    Added config options.

--- __resource.lua
+++ __resource.lua
@@ -1,17 +1,11 @@
 resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 
 client_scripts {
-	"@es_extended/locale.lua",
     "client.lua",
-   	'locales/en.lua',
-	'config.lua',
 }
 
 server_scripts {
 	"server.lua",
-	'@es_extended/locale.lua',
-	'locales/en.lua',
-	'config.lua',
 }
 
 dependencies {
--- server.lua
+++ server.lua
@@ -1,5 +1,9 @@
 ESX = nil
 
+-- EDIT HOW MUCH THE AD COSTS HERE --
+local amount = 200
+
+
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 -- Taxi ad
@@ -7,7 +11,7 @@ RegisterServerEvent('global:taxiad')
 AddEventHandler('global:taxiad', function(args)
 	local xPlayers = ESX.GetPlayers()
 	for i=1, #xPlayers, 1 do
-		TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Downtown Cab Co', 'Reklaam', table.concat(args, " "), 'CHAR_TAXI', 1)
+		TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Downtown Cab Co', 'Advertisement', table.concat(args, " "), 'CHAR_TAXI', 1)
 	end
 end)
 
@@ -16,7 +20,7 @@ RegisterServerEvent('global:mechanicad')
 AddEventHandler('global:mechanicad', function(args)
 	local xPlayers = ESX.GetPlayers()
 	for i=1, #xPlayers, 1 do
-		TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Los Santos Customs', 'Reklaam', table.concat(args, " "), 'CHAR_LS_CUSTOMS', 1)
+		TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Los Santos Customs', 'Advertisement', table.concat(args, " "), 'CHAR_LS_CUSTOMS', 1)
 	end
 end)
 
@@ -25,7 +29,7 @@ RegisterServerEvent('global:globalad')
 AddEventHandler('global:globalad', function(args)
 	local xPlayers = ESX.GetPlayers()
 	for i=1, #xPlayers, 1 do
-		TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Lifeinvader', 'Reklaam', table.concat(args, " "), 'CHAR_LIFEINVADER', 1)
+		TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Lifeinvader', 'Advertisement', table.concat(args, " "), 'CHAR_LIFEINVADER', 1)
 	end
 end)
 
@@ -34,14 +38,13 @@ RegisterServerEvent('global:darkwebad')
 AddEventHandler('global:darkwebad', function(args)
 	local xPlayers = ESX.GetPlayers()
 	for i=1, #xPlayers, 1 do
-		TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'DARKWEB', 'Reklaam', table.concat(args, " "), 'CHAR_LESTER_DEATHWISH', 1)
+		TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'DARKWEB', 'Advertisement', table.concat(args, " "), 'CHAR_LESTER_DEATHWISH', 1)
 	end
 end)
 
 -- Remove money
 RegisterNetEvent("advert:removemoney")
 AddEventHandler("advert:removemoney", function(index)
-    local amount = 200
     local playerMoney = getMoney(source)
     if playerMoney >= amount then
         removeMoney(source, amount)
