f1925e346dc41894689dff27af1e72a12f3f6965
Trickxqster <52986951+Trickxqster@users.noreply.github.com>
2019-11-13 10:29:42 +0200

    Release 2.0
    Updated to Release 2.0

---
+++ locales/en.lua
@@ -0,0 +1,8 @@
+Locales ['en'] = {
+  ['you_paid'] = 'you paid ~g~$%s~s~ for the Advertisement.',
+  ['you_paid_black'] = 'you paid ~r~$%s~s~ for the Dark Web Advertisement.',
+  ['not_enough_money'] = 'You ~r~don\'t~w~ have enough money.',
+  ['no_job'] = 'Your job ~r~doesn\'t~w~ have Advertisement service.',
+  ['no_password'] = 'You ~r~don\'t~w~ have the ~r~password~w~ to use this service.',
+  ['ad_name'] = 'Advertisement.',
+}
